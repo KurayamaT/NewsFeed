cb443fbabc7066ace400c20c37d62f3e4788e177
Aobanomori <t.kurayama@nifty.com>
2020-10-14 00:13:27 +0900

    ちょっと変えました

--- app/src/main/java/net/aobanomori/newsfeed/ui/MainFragment.kt
+++ app/src/main/java/net/aobanomori/newsfeed/ui/MainFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import kotlinx.android.synthetic.main.fragment_main.*
 import net.aobanomori.newsfeed.R
+import net.aobanomori.newsfeed.viewmodel.MainViewModel
 
 
 class MainFragment : Fragment() {
@@ -19,6 +22,17 @@ class MainFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
     }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+        viewModel = ViewModelProvider.of(activity!!).get(MainViewModel::class.java)
+
+        searchButton.setOnClickListener {
+
+        }
+    }
+
 
 
 }
---
+++ app/src/main/java/net/aobanomori/newsfeed/viewmodel/MainViewModel.kt
@@ -0,0 +1,8 @@
+package net.aobanomori.newsfeed.viewmodel
+
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+
+class MainViewModel(private val app: Application): AndroidViewModel(app){
+
+}
